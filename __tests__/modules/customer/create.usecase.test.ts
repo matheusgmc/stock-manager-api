d8c0c0a18bdef202ec7d12144296d44a8a5082bf
matheusgmc <matheus.geraldo1@outlook.com>
2023-02-19 17:23:50 -0400

    :white_check_mark: unit tests to create a new customer

---
+++ __tests__/modules/customer/create.usecase.test.ts
@@ -0,0 +1,43 @@
+import { CreateCustomerUseCase } from "../../../src/modules/customer";
+import { ICreateCustomerRequestDTO } from "../../../src/modules/customer/create/create.dto";
+import { inMemoryCustomerRepository } from "../../repository";
+
+describe("Customer - Create - UseCase", () => {
+  const suit = new CreateCustomerUseCase(inMemoryCustomerRepository);
+  const customerMock: ICreateCustomerRequestDTO = {
+    name: "test",
+  };
+
+  it("should be defined", async () => {
+    expect(suit).toBeDefined();
+    expect(suit.execute).toBeDefined();
+  });
+
+  it("should create a new customer successfully", async () => {
+    const data = await suit.execute(customerMock);
+    expect(data).toHaveProperty("id");
+    expect(data).toHaveProperty("name", customerMock.name);
+  });
+
+  it("should fail if name is empty or undefined", async () => {
+    await expect(
+      suit.execute({
+        name: "",
+      })
+    ).rejects.toHaveProperty("message", "name isn't valid");
+
+    await expect(
+      suit.execute({
+        //@ts-ignore
+        name: undefined,
+      })
+    ).rejects.toHaveProperty("message", "name isn't valid");
+  });
+
+  it("should fail if a customer with the same name already exists", async () => {
+    await expect(suit.execute(customerMock)).rejects.toHaveProperty(
+      "message",
+      "customer with this name already exists"
+    );
+  });
+});
