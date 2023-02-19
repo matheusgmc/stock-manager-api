677012a518c0d9d9ae78edf8daacdd1d39524a62
matheusgmc <matheus.geraldo1@outlook.com>
2023-02-19 17:18:48 -0400

    :clown_face: repository in memory

---
+++ __tests__/repository/in-memory.customer.repository.ts
@@ -0,0 +1,52 @@
+import { randomUUID } from "node:crypto";
+import { CustomerData } from "../../src/database/entities/customer.data.entity";
+import {
+  ICustomerRepository,
+  ICustomerRepositoryCreate,
+  ICustomerRepositoryFindMany,
+  ICustomerRepositoryFindUnique,
+} from "../../src/implements/customer.repository.interface";
+
+const Customers: CustomerData[] = [
+  {
+    id: "1",
+    name: "test_mock_1",
+  },
+  {
+    id: "2",
+    name: "test_mock_2",
+  },
+];
+
+export class InMemoryCustomerRepository implements ICustomerRepository {
+  async create(data: ICustomerRepositoryCreate): Promise<CustomerData> {
+    const newCustomer: CustomerData = {
+      id: randomUUID(),
+      name: data.name,
+    };
+
+    Customers.push(newCustomer);
+
+    return new CustomerData(newCustomer);
+  }
+
+  async findUnique(
+    data: ICustomerRepositoryFindUnique
+  ): Promise<CustomerData | null> {
+    const customer = Customers.find(
+      (elem) => elem.id == data.where.id || elem.name == data.where.name
+    );
+    return customer ? customer : null;
+  }
+
+  async findMany(data: ICustomerRepositoryFindMany): Promise<CustomerData[]> {
+    const customers: CustomerData[] = [];
+
+    Customers.forEach((customer) => {
+      if (customer.name == data.where.name || customer.id == data.where.id) {
+        customers.push(customer);
+      }
+    });
+    return customers;
+  }
+}
--- __tests__/repository/index.ts
+++ __tests__/repository/index.ts
@@ -1,3 +1,5 @@
+import { InMemoryCustomerRepository } from "./in-memory.customer.repository";
 import { InMemoryProductRepository } from "./in-memory.product.repository";
 
 export const inMemoryProductRepository = new InMemoryProductRepository();
+export const inMemoryCustomerRepository = new InMemoryCustomerRepository();
