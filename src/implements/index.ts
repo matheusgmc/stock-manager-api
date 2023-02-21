17b1099dda035f84fecf6677c34266a7336bf246
matheusgmc <matheus.geraldo1@outlook.com>
2023-02-21 00:32:26 -0400

    :sparkles: sale repository abstraction

--- src/implements/index.ts
+++ src/implements/index.ts
@@ -1,3 +1,4 @@
 export * from "./product.repository.interface";
 export * from "./customer.repository.interface";
 export * from "./log.repository.interface";
+export * from "./sale.repository.interface";
---
+++ src/implements/sale.repository.interface.ts
@@ -0,0 +1,13 @@
+import { SaleData } from "../database/entities";
+import { CustomerEntity, PaymentEntity, ProductEntity } from "../entities";
+
+export interface ISaleRepositoryCreate {
+  customer: CustomerEntity;
+  product: ProductEntity;
+  payment: PaymentEntity;
+  created_at: string;
+}
+
+export interface ISaleRepository {
+  create(data: ISaleRepositoryCreate): Promise<SaleData>;
+}
