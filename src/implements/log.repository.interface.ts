3b43cc91092b0f2e4d1e97b517732ca344353345
matheusgmc <matheus.geraldo1@outlook.com>
2023-02-21 14:15:43 -0400

    :recycle: refactor log interface

--- src/implements/log.repository.interface.ts
+++ src/implements/log.repository.interface.ts
@@ -1,11 +1,19 @@
-import { LogData } from "../database/entities/log.data.entity";
-import { CustomerEntity, ProductEntity } from "../entities";
+import { LogData } from "../database/entities";
+import { SaleEntity } from "../entities";
 
 export interface ILogRepositoryCreate {
-  product: ProductEntity;
-  customer: CustomerEntity;
+  sale: SaleEntity;
+  created_at: string;
+}
+
+export interface ILogRepositoryAppend {
+  id: string;
+  sale: SaleEntity;
 }
 
 export interface ILogRepository {
-  create(data: ILogRepositoryCreate): Promise<LogData>;
+  create(data: ILogRepositoryCreate): Promise<void>;
+  append(data: ILogRepositoryAppend): Promise<void>;
+
+  findToday(today: string): Promise<LogData | null>;
 }
