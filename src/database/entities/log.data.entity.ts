d5c4b34905af074ba7209341732e09243c627234
matheusgmc <matheus.geraldo1@outlook.com>
2023-02-21 00:30:18 -0400

    :recycle: log changed

--- src/database/entities/log.data.entity.ts
+++ src/database/entities/log.data.entity.ts
@@ -1,10 +1,13 @@
 import { CustomerData } from "./customer.data.entity";
 import { ProductData } from "./product.data.entity";
+import { SaleData } from "./sale.data.entity";
 
 export class LogData {
   id: string;
-  customer: CustomerData;
-  product: ProductData;
+  sales: SaleData[];
+  created_at: string;
+  updated_at: string;
+
   constructor(props: LogData) {
     Object.assign(this, props);
     Object.freeze(this);
