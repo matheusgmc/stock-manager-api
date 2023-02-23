9ca2e5bc5ee3aaa6dfd5ef305ad41badeacfcee2
matheusgmc <matheus.geraldo1@outlook.com>
2023-02-23 12:19:50 -0400

    :sparkles: added express.json middleware and product routes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
 import express, { Express } from "express";
+import { ProductRoutes } from "./routes/v1";
 export class App {
   app: Express;
   constructor() {
     this.app = express();
+    this.middleware();
+    this.routes();
+  }
+
+  middleware() {
+    this.app.use(express.json());
+  }
+
+  routes() {
+    this.app.use(new ProductRoutes().router);
   }
 
   listen(port: number) {
