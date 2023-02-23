a77f7ed879ace76b1978fff12c80379eff795ef7
matheusgmc <matheus.geraldo1@outlook.com>
2023-02-23 12:08:35 -0400

    :sparkles: prisma repository created for product

---
+++ src/database/repositories/prisma.product.repository.ts
@@ -0,0 +1,52 @@
+import {
+  IProductRepository,
+  IProductRepositoryCreate,
+  IProductRepositoryFindMany,
+  IProductRepositoryFindUnique,
+} from "../../implements";
+import { prisma } from "../../lib/prisma";
+import { ProductData } from "../entities";
+
+export class PrismaProductRepository implements IProductRepository {
+  database = prisma.product;
+
+  async create(data: IProductRepositoryCreate): Promise<ProductData> {
+    const newProduct = await this.database.create({
+      data: {
+        name: data.name,
+        amount: data.amount,
+        price_unit: data.price_unit,
+      },
+    });
+    return new ProductData(newProduct);
+  }
+
+  async findMany(data: IProductRepositoryFindMany): Promise<ProductData[]> {
+    return await this.database.findMany({
+      where: data.where,
+    });
+  }
+  async findUnique(
+    data: IProductRepositoryFindUnique
+  ): Promise<ProductData | null> {
+    return await this.database.findUnique({
+      where: data.where,
+    });
+  }
+  async findAndUpdateName(id: string, name: string): Promise<void> {
+    await this.database.update({
+      where: { id },
+      data: { name },
+    });
+  }
+
+  async findAndUpdateAmount(id: string, amount: number): Promise<void> {
+    await this.database.update({ where: { id }, data: { amount } });
+  }
+  async findAndUpdatePriceUnit(id: string, price_unit: number): Promise<void> {
+    await this.database.update({ where: { id }, data: { price_unit } });
+  }
+  async findByIdAndDelete(id: string): Promise<void> {
+    await this.database.delete({ where: { id } });
+  }
+}
