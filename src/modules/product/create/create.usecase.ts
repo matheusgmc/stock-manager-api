ebb616de79370e8def1ff168833f13495d241fae
matheusgmc <matheus.geraldo1@outlook.com>
2023-02-23 12:03:54 -0400

    :sparkles: error if dto is empty

--- src/modules/product/create/create.usecase.ts
+++ src/modules/product/create/create.usecase.ts
@@ -2,10 +2,15 @@ import { ProductEntity } from "../../../entities/product.entity";
 import { IProductRepository } from "../../../implements/product.repository.interface";
 import { ICreateProductRequestDTO } from "./create.dto";
 
+import { AlreadyExistsError } from "../../errors";
+
 export class CreateProductUseCase {
   constructor(private ProductRepository: IProductRepository) {}
 
   async execute(dto: ICreateProductRequestDTO): Promise<ProductEntity> {
+    if (!dto || Object.entries(dto).length == 0)
+      throw new Error("dto isn't empty");
+
     if (!dto.name || !dto.price_unit) throw new Error("params isn't valid");
 
     if (
@@ -15,7 +20,7 @@ export class CreateProductUseCase {
         },
       })
     )
-      throw new Error("product already exists");
+      throw new AlreadyExistsError("product");
 
     const newProduct = await this.ProductRepository.create({
       price_unit: dto.price_unit,
