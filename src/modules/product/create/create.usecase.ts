9855dcb47399e07488ba3d0ce5212c14166def63
matheusgmc <matheus.geraldo1@outlook.com>
2023-02-23 23:53:37 -0400

    :bug: fix: today's date

--- src/modules/product/create/create.usecase.ts
+++ src/modules/product/create/create.usecase.ts
@@ -22,10 +22,12 @@ export class CreateProductUseCase {
     )
       throw new AlreadyExistsError("product");
 
+    const today = new Date().toLocaleString().split(" ")[0];
     const newProduct = await this.ProductRepository.create({
       price_unit: dto.price_unit,
       amount: dto.amount,
       name: dto.name,
+      created_at: today,
     });
 
     return ProductEntity.create(newProduct);
