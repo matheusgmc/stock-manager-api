3ce39a668dd5fdd35e52a2a965010d1d27b5e110
matheusgmc <matheus.geraldo1@outlook.com>
2023-02-23 23:44:22 -0400

    :sparkles: added new props and validation for props

--- src/entities/product.entity.ts
+++ src/entities/product.entity.ts
@@ -5,12 +5,16 @@ export interface IProductEntityNew {
   name: string;
   price_unit: number;
   amount: number;
+  created_at: string;
+  updated_at: string;
 }
 export class ProductEntity implements IProductEntityNew {
   id: string;
   name: string;
   price_unit: number;
   amount: number;
+  created_at: string;
+  updated_at: string;
 
   constructor(props: IProductEntityNew) {
     Object.assign(this, props);
@@ -18,13 +22,20 @@ export class ProductEntity implements IProductEntityNew {
   }
 
   static create(data: ProductData): ProductEntity {
-    const { name, price_unit, amount: amount, id } = data;
+    const { name, price_unit, amount, id, created_at, updated_at } = data;
+    if (!name) throw new Error("name is empty");
+    if (!price_unit) throw new Error("price_unit is empty");
+    if (!id) throw new Error("id is empty");
+    if (!created_at) throw new Error("created_at is empty");
+    if (!updated_at) throw new Error("updated_at is empty");
 
     return new ProductEntity({
       name,
       price_unit,
       amount,
       id,
+      updated_at,
+      created_at,
     });
   }
 }
