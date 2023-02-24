f97cea9b6c8d60113bb789a242be1d21ebf7fb1e
matheusgmc <matheus.geraldo1@outlook.com>
2023-02-23 23:43:48 -0400

    :sparkles: added created_at and updated_at

--- src/database/entities/product.data.entity.ts
+++ src/database/entities/product.data.entity.ts
@@ -3,12 +3,16 @@ export interface IProductDataNew {
   name: string;
   price_unit: number;
   amount: number;
+  created_at: string;
+  updated_at: string;
 }
 export class ProductData implements IProductDataNew {
   id: string;
   name: string;
   price_unit: number;
   amount: number;
+  created_at: string;
+  updated_at: string;
 
   constructor(props: IProductDataNew) {
     Object.assign(this, props);
