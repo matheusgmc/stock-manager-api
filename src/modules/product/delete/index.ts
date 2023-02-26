c6cdd7d773ff199b122c920a777aef1bb6dc3309
matheusgmc <matheus.geraldo1@outlook.com>
2023-02-26 16:17:47 -0400

    :sparkles: export product module and set repository

--- src/modules/product/delete/index.ts
+++ src/modules/product/delete/index.ts
@@ -1,3 +1,19 @@
+import { PrismaProductRepository } from "database/implements/prisma.product.repository";
+import { DeleteProductController } from "./delete.controller";
 import { DeleteProductUseCase } from "./delete.usecase";
+import { IDeleteProductRequestDTO } from "./delete.dto";
 
-export { DeleteProductUseCase };
+const prismaRepository = new PrismaProductRepository();
+
+const deleteProductUseCase = new DeleteProductUseCase(prismaRepository);
+const deleteProductController = new DeleteProductController(
+  deleteProductUseCase
+);
+
+export {
+  deleteProductUseCase,
+  deleteProductController,
+  DeleteProductController,
+  DeleteProductUseCase,
+  IDeleteProductRequestDTO,
+};
