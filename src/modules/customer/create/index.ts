ff9c428676abcbb2fbbc7b498e80b3e139b6f86c
matheusgmc <matheus.geraldo1@outlook.com>
2023-02-26 17:53:08 -0400

    :sparkles: export controller and usecase

--- src/modules/customer/create/index.ts
+++ src/modules/customer/create/index.ts
@@ -1,6 +1,20 @@
 import { CreateCustomerUseCase } from "./create.usecase";
 import { ICreateCustomerRequestDTO } from "./create.dto";
+import { PrismaCustomerRepository } from "database/implements/prisma.customer.repository";
+import { CreateCustomerController } from "./create.controller";
 
-//export const customerRepository = new Prisma
+export const customerRepository = new PrismaCustomerRepository();
 
-export { CreateCustomerUseCase, ICreateCustomerRequestDTO };
+const createCustomerUseCase = new CreateCustomerUseCase(customerRepository);
+
+const createCustomerController = new CreateCustomerController(
+  createCustomerUseCase
+);
+
+export {
+  createCustomerUseCase,
+  createCustomerController,
+  CreateCustomerController,
+  CreateCustomerUseCase,
+  ICreateCustomerRequestDTO,
+};
