926ab22ffb181f63b41a0eff036bb1d5620eec9c
matheusgmc <matheus.geraldo1@outlook.com>
2023-02-26 03:19:16 -0400

    :technologist: conditional changed if object key is valid

--- src/utils/validation/index.ts
+++ src/utils/validation/index.ts
@@ -6,7 +6,7 @@ export function ObjectKeyIsValid(entity: any, obj: any): boolean {
   return (
     Object.keys(obj).filter((key) =>
       Object.prototype.hasOwnProperty.call(entity, key)
-    ).length === 0
+    ).length > 0
   );
 }
 
