9610eb55cd4a79aadb5cd602c381b154c5815cd0
matheusgmc <matheus.geraldo1@outlook.com>
2023-02-27 08:21:25 -0400

    :necktie: updated validation logic

--- src/modules/sale/history/history.usecase.ts
+++ src/modules/sale/history/history.usecase.ts
@@ -10,18 +10,27 @@ export class HistorySalesUseCase {
   async execute(dto: IHistorySalesRequestDTO): Promise<SaleEntity[] | Error> {
     if (Validation.ObjectIsEmpty(dto))
       return new ParamsRequiredError("date is required");
-    const date_start = new Date(dto.date_start).toISOString();
-    if (date_start == "Invalid Date")
+
+    if (!dto.date_start)
+      return new ParamsRequiredError("date_start is required");
+
+    const date_start = new Date(dto.date_start);
+    if (date_start.toString() == "Invalid Date")
       return new ParamsInvalidError("date_start must be a date valid");
 
     if (dto.date_end && new Date(dto.date_end).toString() == "Invalid Date")
       return new ParamsInvalidError("date_end must be a date valid");
-    const date_end = new Date(dto.date_end).setUTCHours(23, 59, 59);
+
+    let date_end = undefined;
+
+    if (dto.date_end) {
+      date_end = new Date(dto.date_end).setUTCHours(23, 59, 59);
+    }
 
     const sales = await this.SalesRepository.findMany({
       between: {
-        date_start,
-        date_end: dto.date_end ? new Date(date_end).toISOString() : undefined,
+        date_start: date_start.toISOString(),
+        date_end: date_end ? new Date(date_end).toISOString() : date_end,
       },
     });
 
