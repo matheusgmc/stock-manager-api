a0b6d40bbffab5b8db20682c63044b98eb6a893c
matheusgmc <matheus.geraldo1@outlook.com>
2023-02-27 00:07:37 -0400

    :sparkles: added method to find many

--- src/repositories/sale.repository.interface.ts
+++ src/repositories/sale.repository.interface.ts
@@ -7,7 +7,12 @@ export interface ISaleRepositoryFindUniqueData {
   where: Partial<Pick<SaleEntity, "id">>;
 }
 
+export interface ISaleRepositoryFindManyData {
+  where?: Partial<Omit<SaleEntity, "id">>;
+}
+
 export interface ISaleRepository {
   create(data: ISaleRepositoryCreate): Promise<SaleData>;
   findUnique(data: ISaleRepositoryFindUniqueData): Promise<SaleData | null>;
+  findMany(data: ISaleRepositoryFindManyData): Promise<SaleData[]>;
 }
