2cc9af9e75cdeca7c4080a4ed9aff50c2e603380
matheusgmc <matheus.geraldo1@outlook.com>
2023-02-26 23:14:19 -0400

    :sparkles: added repositories

--- src/modules/sale/create/index.ts
+++ src/modules/sale/create/index.ts
@@ -1,5 +1,26 @@
 import { CreateSaleUseCase } from "./create.usecase";
 import { ICreateSaleRequestDTO } from "./create.dto";
 import { CreateSaleController } from "./create.controller";
+import { PrismaSaleRepository } from "database/implements/prisma.sale.repository";
+import { PrismaProductRepository } from "database/implements/prisma.product.repository";
+import { PrismaCustomerRepository } from "database/implements/prisma.customer.repository";
 
-export { CreateSaleController, CreateSaleUseCase, ICreateSaleRequestDTO };
+const saleRepository = new PrismaSaleRepository();
+const productRepository = new PrismaProductRepository();
+const customerRepository = new PrismaCustomerRepository();
+
+const createSaleUseCase = new CreateSaleUseCase(
+  saleRepository,
+  productRepository,
+  customerRepository
+);
+
+const createSaleController = new CreateSaleController(createSaleUseCase);
+
+export {
+  createSaleUseCase,
+  createSaleController,
+  CreateSaleController,
+  CreateSaleUseCase,
+  ICreateSaleRequestDTO,
+};
