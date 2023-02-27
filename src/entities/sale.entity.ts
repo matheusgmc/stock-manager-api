1ae3cafd76f7251adc7a4cd317207cb93b92122f
matheusgmc <matheus.geraldo1@outlook.com>
2023-02-26 22:36:44 -0400

    :sparkles: update entity

--- src/entities/sale.entity.ts
+++ src/entities/sale.entity.ts
@@ -1,20 +1,33 @@
-import { PaymentEntity, ProductEntity, CustomerEntity } from "entities";
+import { PaymentEntity } from "entities";
 
 import { SaleData } from "database/entities";
+import { Validation } from "utils/validation";
+
+type ISaleProduct = {
+  name: string;
+  price_unit: number;
+  quantity_purchased: number;
+};
+
+type ISaleCustomer = {
+  name: string;
+};
 
 export interface ISaleEntityNew {
   id: string;
-  product: ProductEntity;
-  customer: CustomerEntity;
+  product: ISaleProduct;
+  customer: ISaleCustomer;
   payment: PaymentEntity;
+  total_price: number;
   created_at: string;
 }
 
 export class SaleEntity implements ISaleEntityNew {
   id: string;
-  product: ProductEntity;
-  customer: CustomerEntity;
+  product: ISaleProduct;
+  customer: ISaleCustomer;
   payment: PaymentEntity;
+  total_price: number;
   created_at: string;
 
   constructor(props: ISaleEntityNew) {
@@ -25,18 +38,26 @@ export class SaleEntity implements ISaleEntityNew {
   static create(data: SaleData): SaleEntity {
     if (!data) throw new Error("data must not be empty to create a Sale");
 
+    const product: ISaleProduct = {
+      price_unit: data.product_price_unit,
+      quantity_purchased: data.product_quantity_purchased,
+      name: data.product_name,
+    };
+
     const payment = PaymentEntity.create({
       method: data.payment_method,
       status: data.payment_status,
     });
 
-    if (!data.product) throw new Error("product is required to create a Sale");
+    if (Validation.ObjectIsEmpty(product))
+      throw new Error("product is required to create a Sale");
 
-    if (!data.customer)
+    const customer: ISaleCustomer = {
+      name: data.customer_name,
+    };
+
+    if (Validation.ObjectIsEmpty(customer))
       throw new Error("customer is required to create a Sale");
-
-    const product = ProductEntity.create(data.product);
-    const customer = CustomerEntity.create(data.customer);
 
     return new SaleEntity({
       ...data,
