a5360093fad451a94c017ee1020b755cd5b21803
matheusgmc <matheus.geraldo1@outlook.com>
2023-02-26 20:12:26 -0400

    :sparkles: export controller

--- src/modules/customer/find/index.ts
+++ src/modules/customer/find/index.ts
@@ -1,3 +1,18 @@
 import { FindCustomerUseCase } from "./find.usecase";
+import { IFindCustomerRequestDTO } from "./find.dto";
+import { FindCustomerController } from "./find.controller";
+import { PrismaCustomerRepository } from "database/implements/prisma.customer.repository";
 
-export { FindCustomerUseCase };
+const customerRepository = new PrismaCustomerRepository();
+
+const findCustomerUseCase = new FindCustomerUseCase(customerRepository);
+
+const findCustomerController = new FindCustomerController(findCustomerUseCase);
+
+export {
+  findCustomerUseCase,
+  findCustomerController,
+  FindCustomerUseCase,
+  FindCustomerController,
+  IFindCustomerRequestDTO,
+};
