fe93a8173d0769061d0d14c67b923b725abb3c81
matheusgmc <matheus.geraldo1@outlook.com>
2023-02-27 00:17:25 -0400

    :sparkles: added GET method to find sales

--- src/routes/v1/sale.routes.ts
+++ src/routes/v1/sale.routes.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from "express";
 import { createSaleController } from "modules/sale";
+import { findSaleController } from "modules/sale/find";
 
 export class SaleRoutes {
   router = Router();
@@ -9,11 +10,19 @@ export class SaleRoutes {
   }
   initialization() {
     this.router.post(this.path, this.create);
+    this.router.get(this.path, this.find);
   }
 
   async create(req: Request, res: Response) {
     const { data, error, statusCode } = await createSaleController.handle({
       body: req.body,
+    });
+    return res.status(statusCode).json(error ? { error } : data);
+  }
+
+  async find(req: Request, res: Response) {
+    const { data, error, statusCode } = await findSaleController.handle({
+      query: req.query,
     });
     return res.status(statusCode).json(error ? { error } : data);
   }
