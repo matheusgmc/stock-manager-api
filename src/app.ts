391bcde8cf405948ca982cf1556f14b453aa15b2
matheusgmc <matheus.geraldo1@outlook.com>
2023-02-26 20:13:15 -0400

    :sparkles: use customer routes

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express, { Express } from "express";
-import { ProductRoutes } from "./routes/v1";
+import { CustomerRoutes, ProductRoutes } from "./routes/v1";
 export class App {
   app: Express;
   constructor() {
@@ -14,6 +14,7 @@ export class App {
 
   routes() {
     this.app.use(new ProductRoutes().router);
+    this.app.use(new CustomerRoutes().router);
   }
 
   listen(port: number) {
