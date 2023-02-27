3ec4b5ff31fd2156f2a6d9ce69bf5f74fa18264e
matheusgmc <matheus.geraldo1@outlook.com>
2023-02-26 23:21:33 -0400

    :sparkles: export repositories

--- src/database/implements/index.ts
+++ src/database/implements/index.ts
@@ -1,3 +1,7 @@
-export * from "./prisma.product.repository";
-export * from "./prisma.customer.repository";
-export * from "./prisma.sale.repository";
+import { PrismaProductRepository } from "./prisma.product.repository";
+import { PrismaCustomerRepository } from "./prisma.customer.repository";
+import { PrismaSaleRepository } from "./prisma.sale.repository";
+
+export const prismaProductRepository = new PrismaProductRepository();
+export const prismaCustomerRepository = new PrismaCustomerRepository();
+export const prismaSaleRepository = new PrismaSaleRepository();
